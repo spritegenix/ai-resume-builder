6a48825519531156794bbcb623987e4770b1ad0f
pankaj <er.pankaj,k4@gmail.com>
2025-03-27 23:11:05 +0530

    fixes

--- src/app/(main)/templates/page.tsx
+++ src/app/(main)/templates/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Layout from "@/components/layout/Layout";
 import { resumeStyles } from "@/components/ResumeStyles/Styles";
-import Wrapper from "@/components/Wrappers";
 import { getUserSubscriptionLevel } from "@/lib/subscription";
 import { auth } from "@clerk/nextjs/server";
 import { canCreateResume } from "@/lib/permissions";
 import prisma from "@/lib/prisma";
 import TemplateCard from "./TemplateCard";
+import BackStyle1 from "@/components/backgroundStyle/BackStyle1";
 
 export default async function TemplatesPage() {
   const { userId } = await auth();
@@ -22,22 +22,25 @@ export default async function TemplatesPage() {
       getUserSubscriptionLevel(userId),
     ]);
   }
-console.log(userId, totalCount, subscriptionLevel);
+  console.log(userId, totalCount, subscriptionLevel);
   return (
     <Layout>
-      <Wrapper
-        className="flex w-full grid-cols-2 flex-col gap-2 sm:grid md:grid-cols-3 lg:grid-cols-4"
-        isTop2
-      >
-        {resumeStyles.map((style) => (
-          <TemplateCard
-            key={style.id}
-            style={style}
-            canCreate={(subscriptionLevel ? canCreateResume(subscriptionLevel, totalCount) : false)}
-            isUser={userId ? true : false}
-          />
-        ))}
-      </Wrapper>
+      <BackStyle1>
+        <div className="flex w-full grid-cols-2 flex-col gap-2 sm:grid md:grid-cols-3 lg:grid-cols-4">
+          {resumeStyles.map((style) => (
+            <TemplateCard
+              key={style.id}
+              style={style}
+              canCreate={
+                subscriptionLevel
+                  ? canCreateResume(subscriptionLevel, totalCount)
+                  : false
+              }
+              isUser={userId ? true : false}
+            />
+          ))}
+        </div>
+      </BackStyle1>
     </Layout>
   );
 }
