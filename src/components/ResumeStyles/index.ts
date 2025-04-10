bf70b5c5cf2ef61c99020f85dd608d2046419ca0
Ashik <ashikshettyc@gmail.com>
2025-04-10 17:59:58 +0530

    new template

--- src/components/ResumeStyles/index.ts
+++ src/components/ResumeStyles/index.ts
@@ -1,5 +1,5 @@
 // Templates 
 import Stylish1 from "./Stylish1";
 import ATSStyle1 from "@/components/ResumeStyles/ATSStyle1";
-
-export { ATSStyle1, Stylish1 };
+import ATSStyle2 from "@/components/ResumeStyles/ATSStyle2";
+export { ATSStyle1, Stylish1, ATSStyle2 };
