fbff80d3358b05a03d34233cb2c58010b3673b04
Ashik <ashikshettyc@gmail.com>
2025-04-21 17:59:16 +0530

    templete modern2 changed to stylish2

--- src/components/ResumeStyles/Styles.ts
+++ src/components/ResumeStyles/Styles.ts
@@ -8,9 +8,9 @@ import {
     Ats1, Ats2, Ats3, Ats4, Ats5, Ats6, Ats7, Ats8, Ats9, Ats10,
     Stylish1,
     Modern1,
-    Modern2
+    Stylish2
 } from "./index";
-import { a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, st1, m1, m2 } from "@/assets/resume-styles";
+import { a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, st1, st2, m1, } from "@/assets/resume-styles";
 
 interface ResumePreviewProps {
     resumeData: ResumeValues;
@@ -173,17 +173,7 @@ const Modern: ResumeStyle[] = [
         price: "FREE",
         priority: 12,
     },
-    {
-        id: "modern2",
-        name: "Modern Resume",
-        component: Modern2,
-        desc: "Highly ATS Friendly Resume. You can make add or remove your profile photo.",
-        samplePic: m2,
-        category: ["All", "ATS Friendly", "Modern"],
-        tags: ["Multi Page"],
-        price: "FREE",
-        priority: 12,
-    }
+   
 ]
 const Simple: ResumeStyle[] = []
 const Stylish: ResumeStyle[] = [
@@ -198,6 +188,17 @@ const Stylish: ResumeStyle[] = [
         price: "FREE",
         priority: 11
     },
+    {
+        id: "stylish2",
+        name: "Modern Resume",
+        component: Stylish2,
+        desc: "Highly ATS Friendly Resume. You can make add or remove your profile photo.",
+        samplePic: st2,
+        category: ["All", "ATS Friendly", "Modern"],
+        tags: ["Multi Page"],
+        price: "FREE",
+        priority: 12,
+    }
 ]
 
 
--- src/components/ResumeStyles/index.ts
+++ src/components/ResumeStyles/index.ts
@@ -16,18 +16,18 @@ import Ats14 from "./Ats/Ats14";
 import Ats15 from "./Ats/Ats15";
 // Stylish 
 import Stylish1 from "./Stylish/Stylish1";
-
+import Stylish2 from "./Stylish/Stylish2";
 // Simple
 
 
 // Modern
 import Modern1 from "./Modern/Modern1";
-import Modern2 from "./Modern/Modern2";
+
 // Creative
 
 // --------------------------------------------------------------- //
 export {
     Ats1, Ats2, Ats3, Ats4, Ats5, Ats6, Ats7, Ats8, Ats9, Ats10, Ats11, Ats12, Ats13, Ats14, Ats15,
-    Stylish1,
-    Modern1,Modern2
+    Stylish1,Stylish2,
+    Modern1
 };
